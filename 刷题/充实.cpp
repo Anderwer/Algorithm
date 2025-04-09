15a637f9e92a76a331f9f92490026d4767296d9a
anderwer <a15960204413@qq.com>
2025-04-09 22:05:00 +0800

    update

---
+++ 刷题/充实.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using i64 = long long;
+using namespace std;
+
+void dfs(int u, int fa, int GCD, int& cnt, vector<vector<int>>& g, vector<int>& a)
+{
+    if(u != 1) GCD = gcd(GCD, abs(a[u] - a[fa]));
+    if(g[u].empty() && (GCD & (GCD - 1)) == 0) cnt++;
+    for(auto v : g[u]) dfs(v, u, GCD, cnt, g, a);
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> g(n + 1);
+    for(int i = 2; i <= n; i++)
+    {
+        int fa; cin >> fa;
+        g[fa].push_back(i);
+    }
+    vector<int> a(n + 1);
+    for(int i = 1; i <= n; i++) cin >> a[i];
+
+    int ans = 0;
+    dfs(1, 0, 0, ans, g, a);
+    cout << ans << "\n";
+}
+// 0000100000 ----> 0000011111
+
+// 分析操作的本质: 当从集合中任选 2 个奇偶性相同的数 x 和 y, 操作后产生的数正好是两者的 "中点"
+// 记 d = y - x 为这两者的差, 取中点后, 这个差就被 "二分" 为 d / 2 了, 如果后来再对新生成的数和原数继续操作, 效果就是不断将原本的间隔 / 2
+// 要使得集合变为 "充实的", 则要让每对相邻元素的差都是 1
+// 由于每次选的 x, y 奇偶性相同, 则 d 一定是偶数, 因此如果一个间隔 d 能经过若干次 / 2 变成 1, 那么 d 必须是 1 乘上一些 2 的幂次, 也就是说 d 必须是 2^k 形式
+
+// 因此, 如果任意两个奇偶性相同的数的差 d 都是 2 的次幂, 则可以构成 "充实的" 集合
+// 假设现在有三个相邻的数 a[i], a[i + 1], a[i + 2] 且 a[i] < a[i + 1] < a[i + 2]
+// 如果 a[i] 和 a[i + 1] 奇偶性相同, 则有 d[i] = a[i + 1] - a[i] = 2 的次幂
+// 如果 a[i] 和 a[i + 1] 奇偶性不同且 a[i + 1] 和 a[i + 2] 奇偶性不同, 则必有 a[i] 和 a[i + 2] 奇偶性相同, 则有 a[i + 2] - a[i] = 2 的次幂
+// 又因为 a[i + 2] - a[i] = (a[i + 2] - a[i + 1]) + (a[i + 1] - a[i]) = d[i + 1] + d[i] = 2 的次幂
+// 因此, 两种情况分别是 d[i] = 2^k 或者 d[i] + d[i + 1] = 2^k
+
+// 现在分析局部 gcd, 设 g = gcd(d[i], d[i + 1])
+// 由于整除理论, g | d[i] 且 g | d[i + 1], 那么必然有 g | (d[i] + d[i + 1]), 又因为上述证明了 d[i] + d[i + 1] 也是 2 的次幂形式
+// 因此, g 必然是一个 2 的次幂形式
+// 推广到整个集合, 有 g = gcd(d[1], d[2], ..., d[n - 1]) = 2 的次幂
+// 综上, 我们证明了 "任意两个奇偶性相同的数的差 d 都是 2 的次幂" 等价于 gcd(d[1], d[2], ..., d[n - 1]) = 2 的次幂
+
+// 因此, 对于从根到叶子的一条路径上, 我们只需要在 dfs 时记录它从根到叶子的点权差分 gcd, 到达叶子的时候判断一下 gcd 是否是 2 的幂次即可
+ 
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
--- 刷题/童年.cpp
+++ 刷题/童年.cpp
@@ -1,42 +1,93 @@
 #include <bits/stdc++.h>
 using i64 = long long;
 using namespace std;
+const int N = 1e5 + 10;
+vector<int> g[N], h[N];
+int id[10][10][10][10];
+int f[N], cnt[N];
+int cur = 0;
 
-map<array<int, 5>, int> vis;
-int dfs(array<int, 5> s) // vis[s] = 1 表示状态 s 玩家 A 必胜
+void add(int v, int xa, int xb, int ya, int yb)
 {
-    if(vis[s]) return vis[s];
-    if(s[0] == s[1] == 0) return vis[s] = 1;
-    if(s[2] == s[3] == 0) return vis[s] = 2;
+    int u = id[ya][yb][xa][xb]; // 后手变先手
+    g[u].push_back(v); // g[i] 存从末态到初态
+    h[v].push_back(u); // h[i] 存初态到末态
+}
 
-    set<int> st;
-    if(s[4] == 1) // 当前为 A 操作
+void init()
+{
+    queue<int> q;
+    for(int xa = 0; xa < 10; xa++)
+        for(int xb = 0; xb < 10; xb++)
+            for(int ya = 0; ya < 10; ya++)
+                for(int yb = 0; yb < 10; yb++)
+                {
+                    id[xa][xb][ya][yb] = ++cur;
+                    if(xa == 0 && xb == 0) f[cur] = 1, q.push(cur); //先手必胜
+                    else if(ya == 0 && yb == 0) f[cur] = 2, q.push(cur); //先手必败
+                }
+
+    for(int xa = 0; xa < 10; xa++)  
+        for(int xb = 0; xb < 10; xb++)
+            for(int ya = 0; ya < 10; ya++)
+                for(int yb = 0; yb < 10; yb++)
+                {
+                    int v = id[xa][xb][ya][yb]; // 枚举当前状态，下一个状态连向当前状态
+                    if(xa && ya) add(v, (xa + ya) % 10, xb, ya, yb);
+                    if(xa && yb) add(v, (xa + yb) % 10, xb, ya, yb);
+                    if(xb && ya) add(v, xa, (xb + ya) % 10, ya, yb);
+                    if(xb && yb) add(v, xa, (xb + yb) % 10, ya, yb);
+                }
+    
+    while(!q.empty())
     {
-        st.insert(dfs({(s[0] + s[2]) % 10, s[1], s[2], s[3], s[4] ^ 1})); // Ax + Bx
-        st.insert(dfs({(s[0] + s[3]) % 10, s[1], s[2], s[3], s[4] ^ 1})); // Ax + By
-        st.insert(dfs({s[0], (s[1] + s[2]) % 10, s[2], s[3], s[4] ^ 1})); // Ay + Bx
-        st.insert(dfs({s[0], (s[1] + s[3]) % 10, s[2], s[3], s[4] ^ 1})); // Ay + By
+        int now = q.front();
+        q.pop();
+        if(f[now] == 1)
+        {
+            for(auto v : g[now]) // 当前是必胜态，需要统计上一个状态是否连的都是必胜态
+            {
+                if(f[v] == 0)
+                {
+                    cnt[v]++;
+                    if(cnt[v] == h[v].size())//如果一个状态连的都是必胜态，则它为必败态
+                    {
+                        f[v] = 2;
+                        q.push(v);
+                    }
+                }
+            }
+        }
+        else 
+        {
+            for(auto v : g[now]) // 当前是必败态，与之相连的上一个状态都是必胜态
+            {
+                if(f[v] == 0)
+                {
+                    f[v] = 1;
+                    q.push(v);
+                }
+            }
+        }
     }
-    else
-    {
-        st.insert(dfs({s[0], s[1], (s[2] + s[0]) % 10, s[3], s[4] ^ 1}));
-        st.insert(dfs({s[0], s[1], (s[2] + s[1]) % 10, s[3], s[4] ^ 1}));
-        st.insert(dfs({s[0], s[1], s[2], (s[3] + s[0]) % 10, s[4] ^ 1}));
-        st.insert(dfs({s[0], s[1], s[2], (s[3] + s[1]) % 10, s[4] ^ 1}));
-    }
-    for(int i = 1; ; i++)
-        if(!st.count(i)) return vis[s] = i;
 }
 
 void solve()
 {
-    array<int, 5> s; // {Ax, Ay, Bx, By, id} id 为 1 表示当前 A 操作
-    cin >> s[0] >> s[1] >> s[2] >> s[3];
-    s[4] = 1;
-
-    int ans = dfs(s);
-    cout << ans << '\n';
+    int xa, xb, ya, yb;
+    cin >> xa >> xb >> ya >> yb;
+    cout << f[id[xa][xb][ya][yb]] << "\n";
 }
+
+// 正难则反，考虑从结束态往前推，由于博弈论的各个状态相当于一张有向图，可以考虑建图
+// 设 f[xa][xb][ya][yb] 表示先手状态为 xa，xb， 后手状态为 ya，yb时是否为必胜态
+// 初始时 f[0][0][all][all] = 1, f[all][all][0][0] = 2, 注意修改 f[0][0][0][0] = 1
+// 将所有状态进行编号，然后建图，对于当前状态 {xa, xb, ya, yb} 考虑转移
+// 可以转移到 {(xa + ya) % 10, xb} {(xa + yb) % 10, xb} {xa, (xb + ya) % 10} {xa, (xb + yb) % 10}
+
+// 如果当前是必败态，由于双方足够聪明，则所有与它相连的状态都是必胜态
+// 如果当前是必胜态，则让与它相连的所有cnt[v] + 1, 当一个节点的 cnt[v] == 入度 时，它为必败态
+
 
 int main()
 {
@@ -44,6 +95,7 @@ int main()
     std::cin.tie(nullptr);
     int t = 1;
     cin >> t;
+    init();
     while(t--)
     {
         solve();
